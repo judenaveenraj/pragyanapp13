80423f853fef037365288f5e76283ef5cd7cf910
Jasjit Singh Marwah <jasjitsm@gmail.com>
2013-01-22 13:24:46 +0530

    Added Fragment Activity with 3 pages. Currently using dummy fragments.

--- android/gen/com/pragyancsg/pragyanapp13/R.java
+++ android/gen/com/pragyancsg/pragyanapp13/R.java
@@ -16,11 +16,12 @@ public final class R {
     public static final class id {
         public static final int EventTitleView=0x7f070000;
         public static final int eventImageView=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int menu_settings=0x7f070003;
+        public static final int pager=0x7f070002;
     }
     public static final class layout {
-        public static final int activity_pragyan_main=0x7f030000;
-        public static final int event_tile=0x7f030001;
+        public static final int event_tile=0x7f030000;
+        public static final int pragyan_main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_pragyan_main=0x7f060000;
